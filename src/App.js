c45ed8e49c4190a6e1aa3a0af06e1be884411036
kyle <kylelholzinger@gmail.com>
2021-03-17 18:18:06 -0400

    [KH] try out some new styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { hot } from 'react-hot-loader/root'
 import s from './App.module.css'
 import './fonts.css'
 import './reset.css'
+import './index.css'
 
-const App = () => <>
-  <div className={s.title}>ASCII PUNKS</div>
-  <div className={s.body}>Some badass punks doin' badass stuff</div>
-</>
+const App = () => (
+  <div className={s.background}>
+    <div className={s.title}>ASCII PUNKS</div>
+    <div className={s.body}>Some badass punks doin' badass stuff</div>
+  </div>
+)
 
 export default hot(App)
