021a35faa1033960c895bf9745f1b352079d0fe8
kyle <kylelholzinger@gmail.com>
2021-03-17 17:32:30 -0400

    [KH] remove default browser css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import s from './App.module.css'
 import './fonts.css'
+import './reset.css'
 
-const App = () => <div>
+const App = () => <>
   <div className={s.title}>ASCII PUNKS</div>
   <div className={s.body}>Some badass punks doin' badass stuff</div>
-</div>
+</>
 
 export default hot(App)
