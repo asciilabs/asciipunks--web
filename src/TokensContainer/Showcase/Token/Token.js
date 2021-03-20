69dd96e2de091e7a25fa4ffd8b3df5ff697a5f99
kyle <kylelholzinger@gmail.com>
2021-03-20 15:20:05 -0400

    [KH] update display of tokens

--- src/TokensContainer/Showcase/Token/Token.js
+++ src/TokensContainer/Showcase/Token/Token.js
@@ -3,8 +3,8 @@ import s from './Token.module.css'
 
 const Token = ({ token, id }) => (
   <div className={s.tokenContainer}>
+    <div className={s.tokenId}>#{id}</div>
     <pre className={s.token}>{token}</pre>
-    <div className={s.tokenId}>{id}</div>
   </div>
 )
 
