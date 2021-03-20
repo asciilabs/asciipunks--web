a664b81f9fcdf0ee00418585817fc7b5258447a6
kyle <kylelholzinger@gmail.com>
2021-03-20 13:04:35 -0400

    [KH] fix issues with refactor

--- src/shared/components/Button/Button.js
+++ src/shared/components/Button/Button.js
@@ -2,12 +2,13 @@ import React from 'react'
 import cn from 'classnames'
 import s from './Button.module.css'
 
-const Button = ({ children, className, size = 'medium' }) => (
+const Button = ({ children, className, size = 'medium', onClick }) => (
   <button
     className={cn(className, s.button, {
       [s.large]: size == 'large',
       [s.small]: size == 'small',
     })}
+    onClick={onClick}
   >
     {children}
   </button>
