e85b966c50ebd6f6e05002486629989e70e5a7dc
Nassredean Nasseri <dean@nasseri.io>
2021-03-22 17:26:40 -0400

    wip

--- src/TokensContainer/Showcase/Showcase.js
+++ src/TokensContainer/Showcase/Showcase.js
@@ -6,32 +6,38 @@ import { ContractContext } from '@components/ContractRegisterer'
 import Token from './Token'
 import s from './Showcase.module.css'
 
-const tokenIds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const tokenIds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const Showcase = () => {
   const { contract } = useContext(ContractContext)
   const [tokens, setTokens] = useState([])
+  const [tokenLimit, setTokenLimit] = useState(0)
+  const [totalSupply, setTotalSupply] = useState(0);
   const [loaded, setLoaded] = useState(false)
 
   useEffect(async () => {
     if (!contract || loaded) return
 
     setLoaded(true)
-    tokenIds.forEach(async (id) => {
-      const token = formatURI(await contract.methods.tokenURI(id).call())
-      setTokens((tokens) => [...tokens, { token, id }])
-    })
+    // tokenIds.forEach(async (id) => {
+    //   const token = formatURI(await contract.methods.tokenURI(id).call())
+    //   setTokens((tokens) => [...tokens, { token, id }])
+    // })
+    const totalSupply = await contract.methods.totalSupply().call()
+    console.log(totalSupply);
+    const tokenLimit = await contract.methods.TOKEN_LIMIT().call()
+    console.log(tokenLimit);
+    setTotalSupply(totalSupply);
+    setTokenLimit(tokenLimit);
   }, [contract])
 
   const sortedTokens = useMemo(() => sortBy('id', tokens), [tokens])
 
-  if (sortedTokens.length < tokenIds.length) return null
+  // if (sortedTokens.length < tokenIds.length) return null
 
   return (
     <Card className={s.showcase}>
-      {sortedTokens.map(({ token, id }) => (
-        <Token token={token} id={id} key={id} />
-      ))}
+      There are {tokenLimit} tokens in total. So far {totalSupply} have been minted.
     </Card>
   )
 }
