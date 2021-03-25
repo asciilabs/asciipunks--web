7e7be580c66a5657e934320ff6144275b05f0ef9
Nassredean Nasseri <dean@nasseri.io>
2021-03-25 08:01:18 -0400

    [NN] updates to intro copy, add links to generated event, break up cards

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -15,39 +15,62 @@ const Intro = () => (
     <MintedTokens />
 
     <Card className={s.card}>
+      <h2 className={s.h2}>About</h2>
       <p className={s.p}>
-        These little dudes are 12x12 lines of ascii text. But the real beauty of
-        them is that they are generated entirely on-chain! Thats right! Each
-        little punk minted is self contained on the ethereum blockchain. In other
-        words, the NFT itself is the art.
+        These little dudes are comprised of 12x12 lines of ascii text generated entirely on-chain! Thats right! When each
+        little punk is minted, a generative algorithm is run to produce a random punk, which you
+        can see an example of {' '}
+        <a href={process.env.GENERATED_EVENT_URL} target="_blank">here</a>.{' '}
+        Each punk is self contained on the ethereum blockchain. In other words, the NFT itself is the art.
       </p>
       <p className={s.p}>
         The AsciiPunks grew out of a realization that many of the NFT's we trade
         and collect today are nothing more than tokens with a hardcoded link to a
-        centralized server containing metadata. With many NFT's today, you have to
+        centralized server containing metadata. With many NFT's on the market, you have to
         trust that whoever is hosting the actual image keeps their promise to host
-        the artwork for the entire life of the ethereum blockchain. What value can
-        an NFT really have, so long as the token itself is not inseprable from the
-        art in question?
+        the artwork for the entire life of the ethereum blockchain.
+      </p>
+
+      <p className={s.p}>
+        As an on-chain maximalist I find the idea of trusting centralized services to host artwork that I own entirely too custodial,
+        and against the overall ethos of crypto and decentralization.
+        What value can an NFT really have, so long as the token itself is not inseprable from the art in question?
+        It is this question that led to the development of AsciiPunks.
+      </p>
+
+      <p className={s.p}>
+        So if you are an on-chain maximalist, a decentralization junkie, or a real OG crypto head, this is the project for you.
       </p>
     </Card>
 
     <Card className={s.card} id="specs">
       <h2 className={s.h2}>Specs</h2>
       <p className={s.p}>
-        To get a little more technical for the folks who are interested, the
+        To get a little more technical for the folks who are interested,
         AsciiPunks is an ERC721 compatible smart contract on the ethereum
-        blockchain, or in other words an "NFT". When a user mints an AsciiPunk a
+        blockchain, or in other words an "NFT".
+      </p>
+      <p className={s.p}>
+        When a user mints an AsciiPunk a
         generative algorithm randomly produces the ascii-art representing the
-        punk, and emits an event that contains it. You can see an example of that{' '}
-        <a href="">here</a>. After a punk has been minted, at anytime someone can
+        punk, and emits an event that contains it. You can see an example of {' '}
+        <a href={process.env.GENERATED_EVENT_URL} target="_blank">here</a>.{' '}
+      </p>
+      <p className={s.p}>
+        After a punk has been minted, at anytime someone can
         query the smart contract on-chain, and call "draw" with the given tokenID.
         Calling draw will return the tokens ascii art, and since the draw function
         is a view call, it doesn't cost gas to call. Thus anyone can view the art
         on chain at anytime, for free. Hence, with AsciiPunks the token is
         inseparable from the artwork, and its value is linked intrinsically to the
-        ethereum blockchain. So long as the ethereum blockchain survices, so to
+        ethereum blockchain. So long as the ethereum blockchain survives, so to
         will the asciipunks.
+      </p>
+      <p className={s.p}>
+        As a sort of "fallback" mechanism, and to ensure that the AsciiPunks are easily viewable on all other NFT platforms,
+        we also expose a standard ERC721-Metadata compliant `tokenURI` function. This tokenURI function returns a url to our server,
+        which responds with metadata about the punk (including an svg image representing the punk). However this function is not necessary
+        to view the punks on-chain.
       </p>
       <p className={s.p}>
         There will only be 2048 of these little punks ever minted, so get yours
@@ -56,12 +79,13 @@ const Intro = () => (
     </Card>
 
     <Card className={s.card}>
+      <h2 className={s.h2}>Who are we</h2>
       <p className={s.p}>
         AsciiPunks were crafted with love by{' '}
         <a href="https://twitter.com/nassredean" target="_blank">
           nassredean
         </a>
-        , a software degen who quit his job for the blockchain, and{' '}
+        , the most degen software dev you will ever meet, and{' '}
         <a href="https://twitter.com/kyleholzinger" target="_blank">
           kyle
         </a>
