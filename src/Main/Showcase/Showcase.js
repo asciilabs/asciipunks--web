28e540a45ae0563fb82e490752b64beff6296e40
Nassredean Nasseri <dean@nasseri.io>
2021-03-27 10:30:30 -0400

    [NN] When user has no punks, "My Punks" section should display, and say "you dont have any punks yet

--- src/Main/Showcase/Showcase.js
+++ src/Main/Showcase/Showcase.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext, useMemo } from 'react'
+import useWeb3 from '@hooks/useWeb3'
 import { ContractsContext } from '@contexts/Contracts'
 import sortBy from 'lodash/fp/sortBy'
 import Card from '@components/Card'
@@ -8,7 +9,14 @@ import Token from '@components/Token'
 import PunkCount from '../PunkCount'
 import s from './Showcase.module.css'
 
+const PunkList = ({ nfts }) => (
+  nfts.map(({ punk, id }) => (
+    <Token token={punk} id={id} key={id} />
+  ))
+)
+
 const Showcase = () => {
+  const { connected } = useWeb3()
   const { nfts, tokenLimit, totalSupply } = useContracts()
 
   return (
@@ -18,9 +26,11 @@ const Showcase = () => {
         <PunkCount className={s.punkCount} />
       </div>
       <div className={s.showcase}>
-        {nfts.map(({ punk, id }) => (
-          <Token token={punk} id={id} key={id} />
-        ))}
+        {
+          connected && nfts.length > 0 ?
+            <PunkList nfts={nfts}/> :
+            'You don\'t have any punks yet :('
+        }
       </div>
     </Card>
   )
