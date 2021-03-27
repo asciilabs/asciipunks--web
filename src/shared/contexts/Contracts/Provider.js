49e2b4306a84e8f7d80552d1257ada2eb6325c92
kyle <kylelholzinger@gmail.com>
2021-03-27 14:32:37 -0400

    [KH] avoid initial error if contract isn't deployed yet

--- src/shared/contexts/Contracts/Provider.js
+++ src/shared/contexts/Contracts/Provider.js
@@ -22,23 +22,26 @@ const Provider = ({ children }) => {
   const { wallet, walletAddress } = useWeb3()
 
   useEffect(() => {
-    (async function () {
+    ;(async function () {
       if (!punks) return
 
-      await punks.deployed()
-      punks.on('Generated', (tokenId, address, token, { transactionHash }) => {
-        const noty = new Noty({
-          layout: 'bottom',
-          buttons: [
-            Noty.button('Etherscan', cn(s.button, s.small), () =>
-              window.open(
-                `${
-                  process.env.ETHERSCAN_BASE || 'https://etherscan.com'
-                }/tx/${transactionHash}`,
-                '_blank'
-              )
-            ),
-            /*
+      try {
+        await punks.deployed()
+        punks.on(
+          'Generated',
+          (tokenId, address, token, { transactionHash }) => {
+            const noty = new Noty({
+              layout: 'bottom',
+              buttons: [
+                Noty.button('Etherscan', cn(s.button, s.small), () =>
+                  window.open(
+                    `${
+                      process.env.ETHERSCAN_BASE || 'https://etherscan.com'
+                    }/tx/${transactionHash}`,
+                    '_blank'
+                  )
+                ),
+                /*
             Noty.button('Tweet', cn(s.button, s.small), () => {
               const encodedPunk = encodeURIComponent(token)
               const previewLink = `https://carbon.now.sh/?l=txt&code=${encodedPunk}&fm=Fira%20Code`
@@ -48,12 +51,14 @@ const Provider = ({ children }) => {
               window.open(tweetLink, '_blank')
             }),
             */
-          ],
-          text: `Punk #${tokenId} minted— your punk is ready!
+              ],
+              text: `Punk #${tokenId} minted— your punk is ready!
           <div style="padding: 16px 0; display: flex; justify-content: center;"><pre style="display: inline-block;">${token}</pre></div>`,
-          timeout: 4000,
-        }).show()
-      })
+              timeout: 4000,
+            }).show()
+          }
+        )
+      } catch (e) {}
     })()
 
     return () => punks.off('Generated')
