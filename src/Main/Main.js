047e3b0446f14d028ba39746904de37e333ad104
kyle <kylelholzinger@gmail.com>
2021-03-28 17:13:52 -0400

    [KH] fix spelling mistakes

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -16,13 +16,13 @@ const Intro = () => {
   return (
     <>
       <Card className={s.card}>
-        <h2 className={s.h2}>What are the AsciiPunks?</h2>
+        <h2 className={s.h2}>What are the ASCIIPunks?</h2>
         <p className={s.p}>
-          Basically AsciiPunks are what would happen if the CryptoPunks fell
+          Basically ASCIIPunks are what would happen if the CryptoPunks fell
           into a portal and wound up in the terminal dimension!
         </p>
         <p className={s.p}>
-          These little dudes are comprised of 12x12 lines of ascii text
+          These little dudes are comprised of 12x12 lines of ASCII text
           generated entirely on-chain! Thats right! When each little punk is
           minted, a generative algorithm is run to produce a random punk, which
           you can see an example of{' '}
@@ -40,7 +40,7 @@ const Intro = () => {
       <Card className={s.card}>
         <h2 className={s.h2}>Pricing</h2>
         <p className={s.p}>
-          There will be 2048 AsciiPunks minted. They are priced on the following
+          There will be 2048 ASCIIPunks minted. They are priced on the following
           curve:
           <br />
           <br />
@@ -58,7 +58,7 @@ const Intro = () => {
       <Card className={s.card} id="about">
         <h2 className={s.h2}>About</h2> 
         <p className={s.p}>
-          The AsciiPunks grew out of a realization that many of the NFT's we
+          The ASCIIPunks grew out of a realization that many of the NFT's we
           trade and collect today are nothing more than tokens with a hardcoded
           link to a centralized server containing metadata. With many NFT's on
           the market, you have to trust that whoever is hosting the actual image
@@ -70,9 +70,9 @@ const Intro = () => {
           As an on-chain maximalist I find the idea of trusting centralized
           services to host artwork that I own entirely too custodial, and
           against the overall ethos of crypto and decentralization. What value
-          can an NFT really have, so long as the token itself is not inseprable
+          can an NFT really have, so long as the token itself is not inseparable
           from the art in question? It is this question that led to the
-          development of AsciiPunks.
+          development of ASCIIPunks.
         </p>
 
         <p className={s.p}>
@@ -90,29 +90,29 @@ const Intro = () => {
         <p>
           What have you been living under a rock or something? NFT stands for
           "non-fungible token". It is a special type of token minted on the
-          Ethereum blockahain. Each token is unique and can not be replace by
+          Ethereum blockchain. Each token is unique and can not be replace by
           any other token. Lately NFT's have taken the world by storm, as more
           and more people use them to collect and trade there favorite digital
-          art. Think about them as digital collectibles, whose uniquness and
+          art. Think about them as digital collectibles, whose uniqueness and
           authenticity is verified by the blockchain.
         </p>
 
-        <h3 className={cn(s.h3, s.pt)}> > How can I buy an AsciiPunk?</h3>
+        <h3 className={cn(s.h3, s.pt)}> > How can I buy an ASCIIPunk?</h3>
         <p>
-          To buy an AsciiPunk, you need to have an Ethereum wallet setup. A
+          To buy an ASCIIPunk, you need to have an Ethereum wallet setup. A
           commonly recommended one is{' '}
           <a href="https://metamask.io/" target="_blank">
             Metamask
           </a>
           . Get the Metamask chrome extension, set up your wallet, and fund it
-          with ETH. Once you do that, you can use the "connect" button a the top
+          with ETH. Once you do that, you can use the "connect" button at the top
           of the page to connect your wallet to the site. Then mint to your
           hearts content!
         </p>
-        <h3 className={cn(s.h3, s.pt)}> > How can I see my AsciiPunk?</h3>
+        <h3 className={cn(s.h3, s.pt)}> > How can I see my ASCIIPunk?</h3>
         <p>
           After you use the site to mint, and once the transaction is confirmed,
-          your newly adoped punk will show up here. Since AsciiPunks are an
+          your newly adopted punk will show up here. Since ASCIIPunks are an
           ERC721 compliant NFT, they will be viewable/tradeable on all major
           platforms.
         </p>
@@ -122,12 +122,12 @@ const Intro = () => {
         <h2 className={s.h2}>Specs</h2>
         <p className={s.p}>
           To get a little more technical for the folks who are interested,
-          AsciiPunks is an ERC721 compatible smart contract on the ethereum
+          ASCIIPunks is an ERC721 compatible smart contract on the ethereum
           blockchain, or in other words an "NFT".
         </p>
         <p className={s.p}>
-          When a user mints an AsciiPunk a generative algorithm randomly
-          produces the ascii-art representing the punk, and emits an event that
+          When a user mints an ASCIIPunk a generative algorithm randomly
+          produces the ASCII-art representing the punk, and emits an event that
           contains it. You can see an example of{' '}
           <a href={process.env.GENERATED_EVENT_URL} target="_blank">
             here
@@ -137,15 +137,15 @@ const Intro = () => {
         <p className={s.p}>
           After a punk has been minted, at anytime someone can query the smart
           contract on-chain, and call "draw" with the given tokenID. Calling
-          draw will return the tokens ascii art, and since the draw function is
+          draw will return the tokens ASCII art, and since the draw function is
           a view call, it doesn't cost gas to call. Thus anyone can view the art
-          on chain at anytime, for free. Hence, with AsciiPunks the token is
+          on chain at anytime, for free. Hence, with ASCIIPunks the token is
           inseparable from the artwork, and its value is linked intrinsically to
           the ethereum blockchain. So long as the ethereum blockchain survives,
-          so to will the asciipunks.
+          so to will the ASCIIPunks.
         </p>
         <p className={s.p}>
-          As a sort of "fallback" mechanism, and to ensure that the AsciiPunks
+          As a sort of "fallback" mechanism, and to ensure that the ASCIIPunks
           are easily viewable on all other NFT platforms, we also expose a
           standard ERC721-Metadata compliant `tokenURI` function. This tokenURI
           function returns a url to our server, which responds with metadata
@@ -161,7 +161,7 @@ const Intro = () => {
       <Card className={s.card}>
         <h2 className={s.h2}>Who are we</h2>
         <p className={s.p}>
-          AsciiPunks were crafted with love by{' '}
+          ASCIIPunks were crafted with love by{' '}
           <a href="https://twitter.com/nassredean" target="_blank">
             nassredean
           </a>
@@ -169,10 +169,10 @@ const Intro = () => {
           <a href="https://twitter.com/kyleholzinger" target="_blank">
             kyle
           </a>
-          , a wannabe renaisance man with a passion for crypto and art.
+          , a wannabe renaissance man with a passion for crypto and art.
         </p>
         <p className={s.p}>
-          AsciiPunks is not affiliated with Larva Labs, but we thank them for
+          ASCIIPunks is not affiliated with Larva Labs, but we thank them for
           providing the crypto-art space so much inspiration.
         </p>
       </Card>
