9f333bf11e94a191889e506e12dbbc32b2394526
kyle <kylelholzinger@gmail.com>
2021-03-29 01:16:54 -0400

    [KH] social preview for mypunks view

--- src/MyPunks/MyPunks.js
+++ src/MyPunks/MyPunks.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
 import useContracts from '@hooks/useContracts'
 import Token from '@components/Token'
 import Card from '@components/Card'
@@ -16,14 +17,36 @@ const MyPunks = () => {
   }, [punksForUser])
 
   return (
-    <Card className={s.card}>
-      <h2 className={s.h2}>Punks owned by {address}</h2>
-      <div className={s.container}>
-        {nfts.map(({ punk, id }) => (
-          <Token token={punk} id={id} key={id} />
-        ))}
-      </div>
-    </Card>
+    <>
+      <Helmet>
+        <meta
+          name="twitter:image"
+          content={`https://api.asciipunks.com/mypunks/${address}/preview`}
+        />
+        <meta
+          name="twitter:title"
+          content={`ASCIIPunks owned by  #${address}`}
+        />
+        <meta
+          name="og:url"
+          content={`https://asciipunks.com/mypunks/${address}`}
+        />
+        <meta name="og:title" content={`ASCIIPunks owned by #${address}`} />
+        <meta
+          name="og:image"
+          content={`https://api.asciipunks.com/mypunks/${address}/preview`}
+        />
+      </Helmet>
+
+      <Card className={s.card}>
+        <h2 className={s.h2}>Punks owned by {address}</h2>
+        <div className={s.container}>
+          {nfts.map(({ punk, id }) => (
+            <Token token={punk} id={id} key={id} />
+          ))}
+        </div>
+      </Card>
+    </>
   )
 }
 
