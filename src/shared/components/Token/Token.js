7f2b4b197c0368bfbe4ad7c220baf9724feb6d04
kyle <kylelholzinger@gmail.com>
2021-03-29 17:55:18 -0400

    [KH] add share button to tokens

--- src/shared/components/Token/Token.js
+++ src/shared/components/Token/Token.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import cn from 'classnames'
+import { Link } from 'react-router-dom'
 import s from './Token.module.css'
 
 const Token = ({ token, id, showId = true }) => (
   <div className={s.tokenContainer}>
-    {showId ? <div className={s.tokenId}>#{id}</div> : null}
+    {showId ? (
+      <div className={s.tokenId}>
+        #{id}{' '}
+        <Link to={`/punks/${id}`} target="_blank">
+          <i className={cn('fas fa-external-link-alt', s.shareButton)}></i>
+        </Link>
+      </div>
+    ) : null}
     <pre className={s.token}>{token}</pre>
   </div>
 )
