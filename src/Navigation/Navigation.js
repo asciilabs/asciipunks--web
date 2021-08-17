56da13336ae92a42d72af104cb67072684d219fb
kyle <kyleholzinger@hey.com>
2021-08-17 11:30:59 -0400

    [KH] fix bug where navigation is broken

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import Button from '@components/Button'
+import useWeb3 from '@hooks/useWeb3'
+import addresses from '@shared/addresses'
 import ConnectButton from './ConnectButton'
 import s from './Navigation.module.css'
-import addresses from '@shared/addresses'
 
 const baseURI =
   process.env.ETH_NET == 'rinkeby'
@@ -17,17 +18,43 @@ const scrollTo = (id) => {
     .scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' })
 }
 
+const navigateToAbout = (pathname, history) => {
+  if (pathname.match(/\/(my)?punks/)) {
+    history.push('/')
+    return
+  }
+
+  scrollTo('about')
+}
+
+const navigateToMyPunks = (pathname, history, walletAddress) => {
+  if (pathname.match(/\/(my)?punks/)) {
+    history.push(`/mypunks/${walletAddress}`)
+    return
+  }
+
+  scrollTo('mypunks')
+}
+
 const Navigation = () => {
+  const { walletAddress } = useWeb3()
   const history = useHistory()
+  const { pathname } = useLocation()
 
   return (
     <nav className={s.navigation}>
       <div className={s.spacer} />
-      <Button className={s.button} onClick={() => scrollTo('about')}>
+      <Button
+        className={s.button}
+        onClick={() => navigateToAbout(pathname, history)}
+      >
         About
       </Button>
-      <Button className={s.button} onClick={() => scrollTo('mypunks')}>
-        My Punks 
+      <Button
+        className={s.button}
+        onClick={() => navigateToMyPunks(pathname, history, walletAddress)}
+      >
+        My Punks
       </Button>
       <Button
         className={s.button}
@@ -37,9 +64,11 @@ const Navigation = () => {
       </Button>
       <Button
         className={s.button}
-        onClick={() => window.open('https://opensea.io/collection/asciipunks-v2', '_blank')}
+        onClick={() =>
+          window.open('https://opensea.io/collection/asciipunks-v2', '_blank')
+        }
       >
-       OpenSea 
+        OpenSea
       </Button>
       <ConnectButton />
     </nav>
